e4e1e3172cd8e56b10503a5eeee92797001ac49d
Zorrino <pierrekouyoumdjian@hotmail.fr>
2022-03-16 20:56:06 +0100

    Suppression du .vscode/

--- index.js
+++ index.js
@@ -32,5 +32,5 @@ app.get("/plugins", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(`App listening on port ${port}!`);
 });
