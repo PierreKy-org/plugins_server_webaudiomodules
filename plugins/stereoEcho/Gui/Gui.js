446b397d68ac975c3dc2a6d26e0fec86827b318a
Zorrino <pierrekouyoumdjian@hotmail.fr>
2022-04-15 17:26:10 +0200

    Stereo echo plugin

---
+++ plugins/stereoEcho/Gui/Gui.js
@@ -0,0 +1,308 @@
+import '../utils/webaudio-controls.js'
+
+      const getBaseURL = () => {
+        const base = new URL('.', import.meta.url);
+        return `${base}`;
+      };
+      export default class stereoEchoGui extends HTMLElement {
+              constructor(plug) {
+                 
+        super();
+            this._plug = plug;
+            this._plug.gui = this;
+        console.log(this._plug);
+          
+        this._root = this.attachShadow({ mode: 'open' });
+        this._root.innerHTML = `<style>.my-pedal {animation:none 0s ease 0s 1 normal none running;appearance:none;background:rgb(128, 128, 128) none repeat scroll 0% 0% / contain padding-box border-box;border:1px solid rgb(73, 73, 73);bottom:141.281px;clear:none;clip:auto;color:rgb(33, 37, 41);columns:auto auto;contain:none;content:normal;cursor:auto;cx:0px;cy:0px;d:none;direction:ltr;display:block;fill:rgb(0, 0, 0);filter:none;flex:0 1 auto;float:none;font:16px / 24px -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";gap:normal;grid:none / none / none / row / auto / auto;height:237px;hyphens:manual;inset:57.7188px 1024px 141.281px 212px;isolation:auto;left:212px;margin:2px;marker:none;mask:none;offset:none 0px auto 0deg;opacity:1;order:0;orphans:2;outline:rgb(33, 37, 41) none 0px;overflow:visible;padding:1px;page:auto;perspective:none;position:unset;quotes:auto;r:0px;resize:none;right:1024px;rx:auto;ry:auto;speak:normal;stroke:none;top:57.7188px;transform:none;transition:all 0s ease 0s;visibility:visible;widows:2;width:268px;x:0px;y:0px;zoom:1;};</style>
+<div id="stereoEcho" class="resize-drag my-pedal" style="border: 1px solid rgb(73, 73, 73); text-align: center; display: inline-block; vertical-align: baseline; padding: 1px; margin: 2px; box-sizing: border-box; background-size: contain; box-shadow: rgba(0, 0, 0, 0.7) 4px 5px 6px, rgba(0, 0, 0, 0.2) -2px -2px 5px 0px inset, rgba(255, 255, 255, 0.2) 3px 1px 1px 4px inset, rgba(0, 0, 0, 0.9) 1px 0px 1px 0px, rgba(0, 0, 0, 0.9) 0px 2px 1px 0px, rgba(0, 0, 0, 0.9) 1px 1px 1px 0px; border-radius: 15px; background-color: grey; touch-action: none; width: 268px; position: relative; top: 0px; left: 0px; height: 237px;"><div id="stereoEcho" class="resize-drag" style="border: 1px solid rgb(73, 73, 73); text-align: center; display: inline-block; vertical-align: baseline; padding: 1px; margin: 2px; box-sizing: border-box; background-size: contain; border-radius: 15px; background-color: transparent; touch-action: none; width: 128px; position: absolute; top: 33px; left: 67px; height: 197px;"></div><div class="drag" style="padding: 1px; margin: 1px; text-align: center; display: inline-block; box-sizing: border-box; touch-action: none; position: absolute; top: 69px; left: 71px; width: 130px; height: 78.5px;"><webaudio-knob id="/stereo echo/echo 1000/feedback" src="./img/knobs/slider_green.png" sprites="100" min="0" max="100" step="0.1" width="128" height="40" style="touch-action: none; display: block;"><style>
+
+.webaudioctrl-tooltip{
+  display:inline-block;
+  position:absolute;
+  margin:0 -1000px;
+  z-index: 999;
+  background:#eee;
+  color:#000;
+  border:1px solid #666;
+  border-radius:4px;
+  padding:5px 10px;
+  text-align:center;
+  left:0; top:0;
+  font-size:11px;
+  opacity:0;
+  visibility:hidden;
+}
+.webaudioctrl-tooltip:before{
+  content: "";
+	position: absolute;
+	top: 100%;
+	left: 50%;
+ 	margin-left: -8px;
+	border: 8px solid transparent;
+	border-top: 8px solid #666;
+}
+.webaudioctrl-tooltip:after{
+  content: "";
+	position: absolute;
+	top: 100%;
+	left: 50%;
+ 	margin-left: -6px;
+	border: 6px solid transparent;
+	border-top: 6px solid #eee;
+}
+
+webaudio-knob{
+  display:inline-block;
+  position:relative;
+  margin:0;
+  padding:0;
+  cursor:pointer;
+  font-family: sans-serif;
+  font-size: 11px;
+}
+.webaudio-knob-body{
+  display:inline-block;
+  position:relative;
+  z-index:1;
+  margin:0;
+  padding:0;
+}
+</style>
+<div class="webaudio-knob-body" tabindex="1" touch-action="none" style="background-image: url(&quot;./img/knobs/slider_green.png&quot;); background-size: 128px 4040px; outline: none; width: 128px; height: 40px; background-position: 0px 0px; transform: rotate(0deg);"></div><div class="webaudioctrl-tooltip"></div>
+</webaudio-knob></div><div class="drag" style="padding: 1px; margin: 1px; text-align: center; display: inline-block; box-sizing: border-box; touch-action: none; position: absolute; top: 149.5px; left: 71px; width: 130px; height: 78.5px;"><webaudio-knob id="/stereo echo/echo 1000/millisecond" src="./img/knobs/slider_green.png" sprites="100" min="0" max="1000" step="0.1" width="128" height="40" style="touch-action: none; display: block;"><style>
+
+.webaudioctrl-tooltip{
+  display:inline-block;
+  position:absolute;
+  margin:0 -1000px;
+  z-index: 999;
+  background:#eee;
+  color:#000;
+  border:1px solid #666;
+  border-radius:4px;
+  padding:5px 10px;
+  text-align:center;
+  left:0; top:0;
+  font-size:11px;
+  opacity:0;
+  visibility:hidden;
+}
+.webaudioctrl-tooltip:before{
+  content: "";
+	position: absolute;
+	top: 100%;
+	left: 50%;
+ 	margin-left: -8px;
+	border: 8px solid transparent;
+	border-top: 8px solid #666;
+}
+.webaudioctrl-tooltip:after{
+  content: "";
+	position: absolute;
+	top: 100%;
+	left: 50%;
+ 	margin-left: -6px;
+	border: 6px solid transparent;
+	border-top: 6px solid #eee;
+}
+
+webaudio-knob{
+  display:inline-block;
+  position:relative;
+  margin:0;
+  padding:0;
+  cursor:pointer;
+  font-family: sans-serif;
+  font-size: 11px;
+}
+.webaudio-knob-body{
+  display:inline-block;
+  position:relative;
+  z-index:1;
+  margin:0;
+  padding:0;
+}
+</style>
+<div class="webaudio-knob-body" tabindex="1" touch-action="none" style="background-image: url(&quot;./img/knobs/slider_green.png&quot;); background-size: 128px 4040px; outline: none; width: 128px; height: 40px; background-position: 0px 0px; transform: rotate(0deg);"></div><div class="webaudioctrl-tooltip"></div>
+</webaudio-knob></div><label for="stereo echo" style="display: block; touch-action: none; position: absolute; z-index: 1; width: 264px; left: 2px; top: 3.71875px;" class="drag" contenteditable="false">stereo echo</label><label for="echo 1000" style="display: block; touch-action: none; position: absolute; z-index: 1; width: 124px; left: 72px; top: 39.7188px;" class="drag" contenteditable="false">echo 1000</label><label for="feedback" style="text-align: center; display: block; touch-action: none; position: absolute; z-index: 1; width: 128px; left: 74px; top: 118.219px;" class="drag" contenteditable="false">feedback</label><label for="millisecond" style="text-align: center; display: block; touch-action: none; position: absolute; z-index: 1; width: 128px; left: 74px; top: 198.719px;" class="drag" contenteditable="false">millisecond</label></div>`;
+  
+        this.isOn;
+            this.state = new Object();
+            this.setKnobs();
+            this.setSliders();
+            this.setSwitches();
+            //this.setSwitchListener();
+            this.setInactive();
+            // Change #pedal to .my-pedal for use the new builder
+            this._root.querySelector('.my-pedal').style.transform = 'none';
+            //this._root.querySelector("#test").style.fontFamily = window.getComputedStyle(this._root.querySelector("#test")).getPropertyValue('font-family');
+  
+            // Compute base URI of this main.html file. This is needed in order
+            // to fix all relative paths in CSS, as they are relative to
+            // the main document, not the plugin's main.html
+            this.basePath = getBaseURL();
+            console.log("basePath = " + this.basePath)
+  
+            // Fix relative path in WebAudio Controls elements
+            this.fixRelativeImagePathsInCSS();
+  
+            // optionnal : set image background using a relative URI (relative
+            // to this file)
+        //this.setImageBackground("/img/BigMuffBackground.png");
+          
+        // Monitor param changes in order to update the gui
+        window.requestAnimationFrame(this.handleAnimationFrame);
+      
+              }
+          
+              fixRelativeImagePathsInCSS() {
+                 
+      // change webaudiocontrols relative paths for spritesheets to absolute
+          let webaudioControls = this._root.querySelectorAll(
+              'webaudio-knob, webaudio-slider, webaudio-switch, img'
+          );
+          webaudioControls.forEach((e) => {
+              let currentImagePath = e.getAttribute('src');
+              if (currentImagePath !== undefined) {
+                  //console.log("Got wc src as " + e.getAttribute("src"));
+                  let imagePath = e.getAttribute('src');
+                  e.setAttribute('src', this.basePath + '/' + imagePath);
+                  //console.log("After fix : wc src as " + e.getAttribute("src"));
+              }
+          });
+  
+          let sliders = this._root.querySelectorAll('webaudio-slider');
+          sliders.forEach((e) => {
+              let currentImagePath = e.getAttribute('knobsrc');
+              if (currentImagePath !== undefined) {
+                  let imagePath = e.getAttribute('knobsrc');
+                  e.setAttribute('knobsrc', this.basePath + '/' + imagePath);
+              }
+          });
+
+          // BMT Get all fonts
+          // Need to get the attr font
+          let usedFonts = "";
+          let fonts = this._root.querySelectorAll('label[font]');
+          fonts.forEach((e) => {
+              if(!usedFonts.includes(e.getAttribute("font"))) usedFonts += "family=" + e.getAttribute("font") + "&";
+          });
+          let link = document.createElement('link');
+          link.rel = "stylesheet";
+          if(usedFonts.slice(0, -1)) link.href = "https://fonts.googleapis.com/css2?"+usedFonts.slice(0, -1)+"&display=swap";
+          document.querySelector('head').appendChild(link);
+          
+          // BMT Adapt for background-image
+          let divs = this._root.querySelectorAll('div');
+          divs.forEach((e) => {
+              if('background-image' in e.style){
+                let currentImagePath = e.style.backgroundImage.slice(4, -1);
+                if (currentImagePath !== undefined) {
+                    let imagePath = e.style.backgroundImage.slice(5, -2);
+                    if(imagePath != "") e.style.backgroundImage = 'url(' + this.basePath + '/' + imagePath + ')';
+                }
+              }
+          });
+          
+              }
+          
+              setImageBackground() {
+                 
+      // check if the shadowroot host has a background image
+          let mainDiv = this._root.querySelector('#main');
+          mainDiv.style.backgroundImage =
+              'url(' + this.basePath + '/' + imageRelativeURI + ')';
+  
+          //console.log("background =" + mainDiv.style.backgroundImage);
+          //this._root.style.backgroundImage = "toto.png";
+      
+              }
+          
+              attributeChangedCallback() {
+                 
+            console.log('Custom element attributes changed.');
+            this.state = JSON.parse(this.getAttribute('state'));
+        let tmp = '/PingPongDelayFaust/bypass';
+        
+        if (this.state[tmp] == 1) {
+          this._root.querySelector('#switch1').value = 0;
+          this.isOn = false;
+        } else if (this.state[tmp] == 0) {
+          this._root.querySelector('#switch1').value = 1;
+          this.isOn = true;
+        }
+  
+        this.knobs = this._root.querySelectorAll('.knob');
+        console.log(this.state);
+  
+        for (var i = 0; i < this.knobs.length; i++) {
+          this.knobs[i].setValue(this.state[this.knobs[i].id], false);
+          console.log(this.knobs[i].value);
+        }
+      
+              }
+          handleAnimationFrame = () => {
+        this._root.getElementById('/stereo echo/echo 1000/feedback').value = this._plug.audioNode.getParamValue('/stereo echo/echo 1000/feedback');
+        
+
+        this._root.getElementById('/stereo echo/echo 1000/millisecond').value = this._plug.audioNode.getParamValue('/stereo echo/echo 1000/millisecond');
+        
+window.requestAnimationFrame(this.handleAnimationFrame);
+         }
+      
+              get properties() {
+                 
+        this.boundingRect = {
+            dataWidth: {
+              type: Number,
+              value: null
+            },
+            dataHeight: {
+              type: Number,
+              value: null
+            }
+        };
+        return this.boundingRect;
+      
+              }
+          
+              static get observedAttributes() {
+                 
+        return ['state'];
+      
+              }
+          
+              setKnobs() {
+                 this._root.getElementById("/stereo echo/echo 1000/feedback").addEventListener("input", (e) =>this._plug.audioNode.setParamValue("/stereo echo/echo 1000/feedback", e.target.value));
+this._root.getElementById("/stereo echo/echo 1000/millisecond").addEventListener("input", (e) =>this._plug.audioNode.setParamValue("/stereo echo/echo 1000/millisecond", e.target.value));
+
+              }
+          
+              setSliders() {
+                 
+              }
+          
+              setSwitches() {
+                 
+              }
+          
+              setInactive() {
+                 
+        let switches = this._root.querySelectorAll(".switch webaudio-switch");
+  
+        switches.forEach(s => {
+          console.log("### SWITCH ID = " + s.id);
+          this._plug.audioNode.setParamValue(s.id, 0);
+        });
+      
+              }
+          }
+      try {
+          customElements.define('wap-stereoecho', 
+                                stereoEchoGui);
+          console.log("Element defined");
+      } catch(error){
+          console.log(error);
+          console.log("Element already defined");      
+      }
+      
